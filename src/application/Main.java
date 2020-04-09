4991b76ee8c5ece43477dcba77323fae7b3c910e
tiepnn <tiepnguyenptit@gmail.com>
2020-04-09 15:36:45 +0700

    buoi1

---
+++ src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+
+    public static int bai1(int n){
+        return n*(n+1)/2;
+    }
+
+    public static int bai2(int a, int b){
+        int result = (a+b + Math.abs(a-b))/2;
+        return result;
+    }
+
+    public static void bai3(String name){
+        System.out.println(name);
+    }
+
+}
